1186ab2eb7cdbe311ea88a315482dc97403b3128
Graham Shaw <gdshaw@riscpkg.org>
2019-05-04 19:02:36 +0000

    Added class query_string.

---
+++ horace/query_string.cc
@@ -0,0 +1,90 @@
+// This file is part of libholmes.
+// Copyright 2019 Graham Shaw
+// Redistribution and modification are permitted within the terms of the
+// BSD-3-Clause licence as defined by v3.4 of the SPDX Licence List.
+
+#include <algorithm>
+
+#include "horace/query_string.h"
+
+namespace horace {
+
+static std::string decode(const std::string& encoded) {
+	std::string decoded;
+	decoded.reserve(encoded.length());
+
+	size_t index = 0;
+	while (index != encoded.length()) {
+		size_t percent = encoded.find('%', index);
+		if (percent == std::string::npos) {
+			decoded.append(encoded, index);
+			break;
+		}
+		decoded.append(encoded, index, percent - index);
+
+		index = percent + 1;
+		if ((index + 2 > encoded.length()) ||
+			!isxdigit(encoded[index + 0]) ||
+			!isxdigit(encoded[index + 1])) {
+
+			decoded.append(1, '%');
+			continue;
+		}
+		std::string hex = encoded.substr(index, 2);
+		char c = static_cast<char>(stoi(hex, 0, 16));
+		decoded.append(1, c);
+		index += 2;
+	}
+	return decoded;
+}
+
+query_string::query_string(const std::string& query) {
+	size_t index = 0;
+	while (index != query.length()) {
+		size_t param_end = query.find('&');
+		if (param_end == std::string::npos) {
+			param_end = query.length();
+		}
+		std::string param = query.substr(index, param_end - index);
+		if (param_end = query.length()) {
+			index = param_end;
+		} else {
+			index = param_end + 1;
+		}
+
+		if (!param.empty()) {
+			std::string name(param);
+			std::string value;
+			size_t name_end = param.find('=');
+			if (name_end != std::string::npos) {
+				name = param.substr(0, name_end);
+				value = param.substr(name_end + 1, std::string::npos);
+			}
+			std::replace(name.begin(), name.end(), '+', ' ');
+			std::replace(value.begin(), value.end(), '+', ' ');
+			name = decode(name);
+			value = decode(value);
+			_parameters[name] = value;
+		}
+	}
+}
+
+template<>
+std::optional<std::string> query_string::find<std::string>(const std::string& name) const {
+	auto f = _parameters.find(name);
+	if (f == _parameters.end()) {
+		return std::nullopt;
+	}
+	return f->second;
+}
+
+template<>
+std::optional<long> query_string::find<long>(const std::string& name) const {
+	auto f = _parameters.find(name);
+	if (f == _parameters.end()) {
+		return std::nullopt;
+	}
+	return stol(f->second);
+}
+
+} /* namespace horace */
---
+++ horace/query_string.h
@@ -0,0 +1,42 @@
+// This file is part of libholmes.
+// Copyright 2019 Graham Shaw
+// Redistribution and modification are permitted within the terms of the
+// BSD-3-Clause licence as defined by v3.4 of the SPDX Licence List.
+
+#ifndef LIBHOLMES_HORACE_QUERY_STRING
+#define LIBHOLMES_HORACE_QUERY_STRING
+
+#include <optional>
+#include <map>
+#include <string>
+
+namespace horace {
+
+/** A class to represent a decoded query string.
+ * If two parameters have the same name then the later one
+ * overwrites the earlier one.
+ */
+class query_string {
+private:
+	/** The query parameters. */
+	std::map<std::string, std::string> _parameters;
+public:
+	/** Construct query string.
+	 * @param query the encoded query string
+	 */
+	explicit query_string(const std::string& query);
+
+	/** Find value of parameter.
+	 * The required return type should be specified as a template
+	 * parameter. The only types currently supported are:
+	 * - long
+	 * - std::string
+	 * @param name the name of the parameter to be found
+	 */
+	template<typename T>
+	std::optional<T> find(const std::string& name) const;
+};
+
+} /* namespace horace */
+
+#endif
