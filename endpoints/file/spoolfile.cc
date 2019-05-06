22709f02f64f9ec7cc37d986a7ef3b20edc8f837
Graham Shaw <gdshaw@riscpkg.org>
2019-05-06 07:53:47 +0000

    Added class spoolfile.

---
+++ endpoints/file/spoolfile.cc
@@ -0,0 +1,47 @@
+// This file is part of libholmes.
+// Copyright 2019 Graham Shaw
+// Redistribution and modification are permitted within the terms of the
+// BSD-3-Clause licence as defined by v3.4 of the SPDX Licence List.
+
+#include <stdexcept>
+#include <climits>
+#include <sstream>
+#include <iomanip>
+
+#include "spoolfile.h"
+
+namespace horace {
+
+spoolfile::spoolfile(const std::string& filename):
+	_filename(filename),
+	_filenum(0) {
+
+	if (_filename.empty()) {
+		throw std::invalid_argument("invalid spoolfile name");
+	}
+	for (std::string::const_iterator it = _filename.begin(); it != _filename.end(); ++it) {
+		char c = *it;
+		if ((c < '0') || (c > '9')) {
+			throw std::invalid_argument("invalid spoolfile name");
+		}
+		unsigned long digit = c - '0';
+		if (_filenum > ULONG_MAX / 10) {
+			throw std::out_of_range("filenum overflow");
+		}
+		_filenum *= 10;
+		if (_filenum > ULONG_MAX - digit) {
+			throw std::out_of_range("filenum overflow");
+		}
+		_filenum += digit;
+	}
+}
+
+spoolfile::spoolfile(unsigned long filenum, unsigned int minwidth):
+	_filenum(filenum) {
+
+	std::ostringstream builder;
+	builder << std::setfill('0') << std::setw(minwidth) << _filenum;
+	_filename = builder.str();
+}
+
+} /* namespace */
---
+++ endpoints/file/spoolfile.h
@@ -0,0 +1,57 @@
+// This file is part of libholmes.
+// Copyright 2019 Graham Shaw
+// Redistribution and modification are permitted within the terms of the
+// BSD-3-Clause licence as defined by v3.4 of the SPDX Licence List.
+
+#ifndef LIBHOLMES_HORACE_SPOOLFILE
+#define LIBHOLMES_HORACE_SPOOLFILE
+
+#include <string>
+
+namespace horace {
+
+/** A class to represent a spoolfile. */
+class spoolfile {
+private:
+	/** The filename of the spoolfile. */
+	std::string _filename;
+
+	/** The filenum of the spoolfile. */
+	unsigned long _filenum;
+public:
+	/** Construct spoolfile from filename.
+	 * @param filename the filename
+	 */
+	spoolfile(const std::string& filename);
+
+	/** Construct spoolfile from filenum.
+	 * @param filenum the filenum
+	 * @param minwidth the minimum width
+	 */
+	spoolfile(unsigned long filenum, unsigned int minwidth);
+
+	/** Get the filename of this spoolfile.
+	 * @return the filename
+	 */
+	const std::string& filename() const {
+		return _filename;
+	}
+
+	/** Get the filenum of this spoolfile.
+	 * @return the filenum
+	 */
+	unsigned long filenum() const {
+		return _filenum;
+	}
+
+	/** Test whether the filename is padded.
+	 * @return true if padded, otherwise false
+	 */
+	bool has_padding() const {
+		return _filename.length() && (_filename[0] == '0');
+	}
+};
+
+} /* namespace horace */
+
+#endif
