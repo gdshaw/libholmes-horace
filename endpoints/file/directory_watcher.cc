502aead91ada864d587158e1efe4162245d3e92f
Graham Shaw <gdshaw@riscpkg.org>
2019-05-11 14:36:18 +0000

    Added class directory_watcher.

---
+++ endpoints/file/directory_watcher.cc
@@ -0,0 +1,49 @@
+// This file is part of libholmes.
+// Copyright 2019 Graham Shaw
+// Redistribution and modification are permitted within the terms of the
+// BSD-3-Clause licence as defined by v3.4 of the SPDX Licence List.
+
+#include <climits>
+
+#include <unistd.h>
+#include <sys/inotify.h>
+
+#include "horace/libc_error.h"
+
+#include "directory_watcher.h"
+
+namespace horace {
+
+static const size_t buffer_size = sizeof(struct inotify_event) + NAME_MAX + 1;
+
+directory_watcher::directory_watcher(const std::string& pathname):
+	_fd(inotify_init()),
+	_buffer(std::make_unique<char[]>(buffer_size)),
+	_count(0),
+	_index(0) {
+
+	if (_fd == -1) {
+		throw libc_error();
+	}
+	_fd.interruptible(true);
+	inotify_add_watch(_fd, pathname.c_str(), IN_CREATE|IN_MODIFY);
+}
+
+std::string directory_watcher::read() {
+	while (true) {
+		if (_index < _count) {
+			char* ptr = _buffer.get() + _index;
+			struct inotify_event* event =
+				reinterpret_cast<struct inotify_event*>(ptr);
+			std::string filename(event->name);
+			size_t size = sizeof(struct inotify_event) + event->len;
+			_index += size;
+			return filename;
+		}
+
+		_count = _fd.read(_buffer.get(), buffer_size);
+		_index = 0;
+	}
+}
+
+} /* namespace horace */
---
+++ endpoints/file/directory_watcher.h
@@ -0,0 +1,47 @@
+// This file is part of libholmes.
+// Copyright 2019 Graham Shaw
+// Redistribution and modification are permitted within the terms of the
+// BSD-3-Clause licence as defined by v3.4 of the SPDX Licence List.
+
+#ifndef LIBHOLMES_HORACE_DIRECTORY_WATCHER
+#define LIBHOLMES_HORACE_DIRECTORY_WATCHER
+
+#include <memory>
+#include <string>
+
+#include "horace/file_descriptor.h"
+
+namespace horace {
+
+/** A class for monitoring a directory for newly-created entries. */
+class directory_watcher {
+private:
+	/** The file descriptor used to watch the directory. */
+	file_descriptor _fd;
+
+	/** A buffer for receiving events. */
+	std::unique_ptr<char[]> _buffer;
+
+	/** The number of occupied octets in the buffer. */
+	size_t _count;
+
+	/** The index of the first unused octet in the buffer,
+	 * or _count if there are no unused octet. */
+	size_t _index;
+public:
+	/** Watch directory.
+	 * @param pathname the directory pathname
+	 */
+	directory_watcher(const std::string& pathname);
+
+	/** Read the next file/directory name.
+	 * If there are no names waiting to be read then this function
+	 * will block until one is available.
+	 * @return the name of a newly-created file or directory
+	 */
+	std::string read();
+};
+
+} /* namespace horace */
+
+#endif
