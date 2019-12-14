1de0ae770329ee4ba951226826fe5e669acc95c7
Graham Shaw <gdshaw@riscpkg.org>
2019-12-14 13:28:35 +0000

    Added -A option to horace genkey command for selecting signing algorithm.

--- src/horace-genkey.cc
+++ src/horace-genkey.cc
@@ -3,6 +3,7 @@
 // Redistribution and modification are permitted within the terms of the
 // BSD-3-Clause licence as defined by v3.4 of the SPDX Licence List.
 
+#include <cstring>
 #include <string>
 #include <iostream>
 
@@ -14,28 +15,46 @@
  * @param out the ostream to which the help text should be written
  */
 void write_help(std::ostream& out) {
-        out << "Usage: horace genkey [<options>]" << std::endl;
-        out << std::endl;
-        out << "Options:" << std::endl;
-        out << std::endl;
-        out << "  -h  display this help text then exit" << std::endl;
+	out << "Usage: horace genkey [<options>]" << std::endl;
+	out << std::endl;
+	out << "Options:" << std::endl;
+	out << std::endl;
+	out << "  -A  select digital signature algorithm" << std::endl;
+	out << "  -h  display this help text then exit" << std::endl;
 }
 
 int main(int argc, char* argv[]) {
+	// Initialise default options.
+	const char* signing_algorithm = 0;
+
 	// Parse command line options.
 	int opt;
-	while ((opt = getopt(argc, argv, "h")) != -1) {
+	while ((opt = getopt(argc, argv, "A:h")) != -1) {
 		switch (opt) {
+		case 'A':
+			signing_algorithm = optarg;
+			break;
 		case 'h':
 			write_help(std::cout);
 			return 0;
 		}
 	}
 
+	// Validate algorithm name.
+	if (!signing_algorithm) {
+		signing_algorithm = "ed25519";
+	}
+	if (strcmp(signing_algorithm, "ed25519")) {
+		std::cerr << "signing algorithm not recognised" << std::endl;
+		exit(1);
+	}
+
+	// Generate key.
 	unsigned char pk[crypto_sign_PUBLICKEYBYTES];
 	unsigned char sk[crypto_sign_SECRETKEYBYTES];
 	crypto_sign_keypair(pk, sk);
 
+	// Write key to std::cout in keyfile format.
 	char pk_hex[crypto_sign_PUBLICKEYBYTES*2+1];
 	char sk_hex[crypto_sign_SECRETKEYBYTES*2+1];
 	std::cout << "ed25519:";
